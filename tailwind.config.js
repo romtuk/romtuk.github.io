98183ad972ce5315781d5d388f8726fc6b28bcbd
Ro <rom.tumakov@gmail.com>
2024-08-13 17:31:49 +0300

    Intro for porfolio page. HTML CSS TAILWIND

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  
+  theme: {
+    extend: {
+      colors: {
+        "color0": "var(--color0)",
+        "color1": "var(--color1)",
+        "color2": "var(--color2)",
+        "color3": "var(--color3)",  
+        "color4": "var(--color4)",
+        "color5": "var(--color5)",
+        "color6": "var(--color6)"
+      },
+    },
+  },
+  plugins: [],
+}
+
+ 
